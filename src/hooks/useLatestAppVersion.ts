e3f6b4df785e6a078ed7afd15af5e5e27999d80d
ldecameyer@gmail.com <LucDeCaf>
2023-10-28 19:14:41 +0100

    Added useLatestAppVersion hook + made download page always show latest

---
+++ src/hooks/useLatestAppVersion.ts
@@ -0,0 +1,3 @@
+export default function useLatestAppVersion() {
+    return ['flashcard-city-v1.0.1', '1.0.1'];
+}
--- src/routes/download.tsx
+++ src/routes/download.tsx
@@ -1,6 +1,9 @@
 import Container from "react-bootstrap/Container";
+import useLatestAppVersion from "../hooks/useLatestAppVersion";
 
 export default function Download() {
+  const [latestVersion, versionNumber] = useLatestAppVersion();
+
   return (
     <Container>
       <h1 className="mt-5">Download Flashcard City</h1>
@@ -8,13 +11,13 @@ export default function Download() {
       <br />
       <h3>Download Latest Version for <strong>Windows</strong></h3>
       <ul>
-        <li><strong>V 1.0.1:</strong> <a href="/flashcard-city-v1.0.1.zip" download>flashcard-city-v1.0.1.zip</a></li>
+        <li><strong>V {versionNumber}:</strong> <a href={`/${latestVersion}.zip`} download>{latestVersion}</a></li>
       </ul>
       <br />
       <h5>Download Any Version</h5>
       <ul>
-      <li><strong>V 1.0.1:</strong> <a href="/flashcard-city-v1.0.1.zip" download>flashcard-city-v1.0.zip</a></li>
-        <li><strong>V 1.0:</strong> <a href="/flashcard-city-v1.0.zip" download>flashcard-city-v1.0.zip</a></li>
+      <li><strong>V 1.0.1:</strong> <a href="/flashcard-city-v1.0.1.zip" download>flashcard-city-v1.0</a></li>
+        <li><strong>V 1.0:</strong> <a href="/flashcard-city-v1.0.zip" download>flashcard-city-v1.0</a></li>
       </ul>
     </Container>
   );
