ae76834ef8de0791f498afeb6692501a8ad0c478
ldecameyer@gmail.com <LucDeCaf>
2023-10-28 18:46:08 +0100

    Updated app version + updated download page

--- src/routes/download.tsx
+++ src/routes/download.tsx
@@ -6,9 +6,15 @@ export default function Download() {
       <h1 className="mt-5">Download Flashcard City</h1>
       <p>Install the application for free today!</p>
       <br />
-      <h4>Download Latest Version for <strong>Windows</strong></h4>
+      <h3>Download Latest Version for <strong>Windows</strong></h3>
       <ul>
-        <li>Compressed folder: <a href="/flashcard-city-v1.0.zip" download>flashcard-city-v1.0.zip</a></li>
+        <li><strong>V 1.0.1:</strong> <a href="/flashcard-city-v1.0.1.zip" download>flashcard-city-v1.0.1.zip</a></li>
+      </ul>
+      <br />
+      <h5>Download Any Version</h5>
+      <ul>
+      <li><strong>V 1.0.1:</strong> <a href="/flashcard-city-v1.0.1.zip" download>flashcard-city-v1.0.zip</a></li>
+        <li><strong>V 1.0:</strong> <a href="/flashcard-city-v1.0.zip" download>flashcard-city-v1.0.zip</a></li>
       </ul>
     </Container>
   );
